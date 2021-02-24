a9ef2333b9d134af5680c7fbcd704d1f34e157ea
mstfzdmr <iammustafaozdemir@gmail.com>
2021-02-24 11:37:32 +0300

    webapi project created

---
+++ maskify.api/Controllers/MaskController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace maskify.api.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/mask")]
+    public class MaskController : Controller
+    {
+    }
+}
---
+++ maskify.api/Startup.cs
@@ -0,0 +1,39 @@
+using maskify.core;
+using maskify.core.Extensions;
+using maskify.core.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace maskify.api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped(typeof(IMaskify<>), typeof(Maskify<>));
+            services.AddScoped<IMaskedService, MaskedService>();
+            services.AddScoped<IMasked, Masked>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
