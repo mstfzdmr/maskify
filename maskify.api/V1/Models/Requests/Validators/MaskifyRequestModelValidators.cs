7c855c63e415095de23cc5e029a0b63be70076d9
mstfzdmr <iammustafaozdemir@gmail.com>
2021-10-16 16:55:58 +0300

    request model validators added

---
+++ maskify.api/V1/Models/Requests/Validators/MaskifyRequestModelValidators.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace maskify.api.V1.Models.Requests.Validators
+{
+    public class MaskifyRequestModelValidators : AbstractValidator<MaskifyRequestModel>
+    {
+        public MaskifyRequestModelValidators()
+        {
+            RuleFor(o => o.Model)
+                .NotNull().WithMessage("'Model' must not be null.")
+                .NotEmpty().WithMessage("'Model' must not be empty.");
+
+            RuleFor(o => o.ReplacedJsonKeyValues)
+                .NotNull().WithMessage("'ReplacedJsonKeyValues' must not be null.")
+                .NotEmpty().WithMessage("'ReplacedJsonKeyValues' must not be empty.");
+        }
+    }
+}
